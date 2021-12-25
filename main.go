7487e38006b944a9f6f944effabd3e9912740687
tsar <your.email@example.com>
2021-12-25 15:28:25 +0400

    new game

--- main.go
+++ main.go
@@ -4,42 +4,42 @@ import (
 	"bufio"
 	"fmt"
 	"log"
+	"math/rand"
 	"os"
 	"strconv"
 	"strings"
+	"time"
 )
 
 func main() {
 
-	fmt.Print("Enter a grade: ")
-	reader := bufio.NewReader(os.Stdin)
-	input, err := reader.ReadString('\n')
-	if err !=  nil {
-		log.Fatal(err)
-	}
+	seconds := time.Now().Unix()
+	fmt.Println(seconds)
+	rand.Seed(seconds)
+	target := rand.Intn(100) + 1
+	fmt.Println(target)
 
-	input = strings.TrimSpace(input)
-	grade, err := strconv.ParseFloat(input, 64)
-	if err != nil {
-		log.Fatal(err)
+	render := bufio.NewReader(os.Stdin)
+
+	for guesses := 0; guesses < 10; guesses++ {
+		fmt.Println("You have", 10-guesses, "guesses left.")
+		fmt.Print("Make a guess: ")
+		input, err := render.ReadString('\n')
+		if err != nil {
+			log.Fatal(err)
+		}
+		input = strings.TrimSpace(input)
+		guess, err := strconv.Atoi(input)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		if guess < target {
+			fmt.Println("Your guess was LOW.")
+		} else if guess > target {
+			fmt.Println("Your guess wa HIGHT.")
+		}
+
 	}
-	var status string
-	if grade >= 60 {
-		status = "passing"
-	} else {
-		status = "failing"
-	}
-	fmt.Println(status)
 
 }
-
-/*
-fmt.Println("_______________________")
-	broken := "G# r#cks!"
-	replacer := strings.NewReplacer("#", "o")
-	fixed := replacer.Replace(broken)
-	fmt.Println(fixed)
-
-
-
- */
